79ac099a23757ad3326ae29b2894188e36a664fc
tusharfiet24 <ertusharfiet24@gmail.com>
2024-08-09 11:47:06 +0530

    Add a place and validate the response on dummy google maps api

--- src/Basics.java
+++ src/Basics.java
@@ -1,36 +1,31 @@
 import io.restassured.RestAssured;
+//import io.restassured.path.json.JsonPath;
+
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import files.Payload;
 
 public class Basics {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Validate if Add Place API is working as expected
-		
-		//Given- all input details 
-		//When- submit the API- resource, http method 
-		//Then- validate the response
-		
+
+		// Given- all input details
+		// When- submit the API- resource, http method
+		// Then- validate the response
+		// Add Place -> Update Place with new address -> Get Place to validate if new
+		// address is present in response
+
 		RestAssured.baseURI = "https://rahulshettyacademy.com/";
-		
+
+		System.out.println("============Post Call==============");
 		given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
-		.body("{\r\n"
-				+ "    \"location\": {\r\n"
-				+ "        \"lat\": -38.383494,\r\n"
-				+ "        \"lng\": 33.427362\r\n"
-				+ "    },\r\n"
-				+ "    \"accuracy\": 50,\r\n"
-				+ "    \"name\": \"Tushar Rathore Academy\",\r\n"
-				+ "    \"phone_number\": \"(+91) 983 893 3937\",\r\n"
-				+ "    \"address\": \"29, side layout, cohen 09\",\r\n"
-				+ "    \"types\": [\r\n"
-				+ "        \"shoe park\",\r\n"
-				+ "        \"shop\"\r\n"
-				+ "    ],\r\n"
-				+ "    \"website\": \"http://google.com\",\r\n"
-				+ "    \"language\": \"French-IN\"\r\n"
-				+ "}")
-		.when().post("maps/api/place/add/json")
-		.then().log().all().assertThat().statusCode(200);	
+				.body(Payload.addPlace())
+				.when().post("maps/api/place/add/json")
+				.then().log().all().assertThat()
+				.statusCode(200).body("scope", equalTo("APP")).header("server", "Apache/2.4.52 (Ubuntu)");
+
 	}
 }
