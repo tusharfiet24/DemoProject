4a6f51cc7153ba8b0b5adc0256b94964da836ad7
tusharfiet24 <ertusharfiet24@gmail.com>
2024-08-09 12:38:02 +0530

    Add a ReUsableMethods class which converts raw response to Json response

---
+++ src/files/ReUsableMethods.java
@@ -0,0 +1,10 @@
+package files;
+
+import io.restassured.path.json.JsonPath;
+
+public class ReUsableMethods {
+	public static JsonPath rawToJson(String response) {
+		JsonPath js = new JsonPath(response);
+		return js;
+	}
+}
