0d7c5b8f246b5bb9c3d505b6c057d3ffb7088a67
tusharfiet24 <ertusharfiet24@gmail.com>
2024-08-09 12:31:36 +0530

    Refactor the code and Add a TestNg Assertion

--- src/Basics.java
+++ src/Basics.java
@@ -1,10 +1,11 @@
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
-
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+import org.testng.Assert;
 import files.Payload;
+import files.ReUsableMethods;
 
 public class Basics {
 
@@ -28,22 +29,28 @@ public class Basics {
 				.statusCode(200).body("scope", equalTo("APP")).header("server", "Apache/2.4.52 (Ubuntu)")
 				.extract().response().asString();
 		
-		JsonPath js = new JsonPath(response); // For parsing Json
+		JsonPath js = ReUsableMethods.rawToJson(response); // For parsing Json
 		String placeId = js.getString("place_id");
 		
 		System.out.println("============Put Call==============");
+		String newAddress = "Summer walk, India";
 		given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
 				.body("{\r\n"
 						+ "    \"place_id\": \""+placeId+"\",\r\n"
-						+ "    \"address\": \"70 Summer walk, UK\",\r\n"
+						+ "    \"address\": \""+newAddress+"\",\r\n"
 						+ "    \"key\": \"qaclick123\"\r\n"
 						+ "}")
 				.when().put("maps/api/place/update/json")
 				.then().log().all().assertThat().statusCode(200).body("msg", equalTo("Address successfully updated"));
 		
 		System.out.println("============Get Call==============");
-		given().log().all().queryParam("key", "qaclick123").queryParam("place_id", placeId)
+		String getPlaceResponse = given().log().all().queryParam("key", "qaclick123").queryParam("place_id", placeId)
 				.when().get("maps/api/place/get/json")
-				.then().log().all().assertThat().statusCode(200).body("address", equalTo("70 Summer walk, UK"));
+				.then().log().all().assertThat().statusCode(200).extract().response().asString();
+		
+		JsonPath js1 = ReUsableMethods.rawToJson(getPlaceResponse);
+		String actualAddress = js1.getString("address");
+		
+		Assert.assertEquals(actualAddress, newAddress);
 	}
 }
